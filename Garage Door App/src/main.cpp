0521f297787dec7e7be22ca685346cae9002b7b1
Kobe Maristela <kobe@maristela.net>
2022-08-29 21:48:04 -0700

    Initialized Wifi connection

--- Garage Door App/src/main.cpp
+++ Garage Door App/src/main.cpp
@@ -1,7 +1,40 @@
 #include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+#include <WiFi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <credentials.h>
+
+
+/**
+ * @Setup WiFi Credentials
+ **/
+
+char* ssid = mySSID;
+char* password = myPassword;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(1000);
+
+  /**
+   * Initialize Wi-Fi connection
+  */
+
+  WiFi.begin(ssid, password);
+  Serial.printf("\nConnecting to %s", mySSID);
+
+
+  while(WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      delay(100);
+  }
+
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+
+  
 }
 
 void loop() {
